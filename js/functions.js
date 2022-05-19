788b3032241ebddb2737b7ca8d53cfe6adccd850
marcelobuzzi <marcelobuzziriera@gmail.com>
2022-05-19 20:51:14 -0300

    2022.05.19 - 20:51

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,68 @@
+const regEx = {
+  alpha  : /^[a-zA-Z áéíóúñÑâêîôû]*$/,
+  numeric: /^[0-9]+$/,
+  email  : /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/,
+};
 
+const app = Vue.createApp({
+  data() {
+    return {
+      nombre       : '',
+      email        : '',
+      consulta     : '',
+      nombreError  : '',
+      emailError   : '',
+      consultaError: '',
+    }
+  },
+  methods: {
+    validarNombre() {
+      if(!regEx.alpha.test(this.nombre)) {
+        this.nombreError = 'El nombre no es válido, solo letras.';
+        return false;
+      } else {
+        this.nombreError = '';
+        return true;
+      };
+    },
+    validarEmail() {
+      if(!regEx.email.test(this.email)) {
+        this.emailError = 'El email no es válido.';
+        return false;
+      } else {
+        this.emailError = '';
+        return true;
+      };
+    },
+    validarConsulta() {
+      if(this.consulta.trim() == '') {
+        this.consultaError = 'La consulta no puede estar vacía.';
+        return false;
+      } else {
+        this.consultaError = '';
+        return true;
+      };
+    },
+    validar() {
+      if(this.nombre.trim() != '' && this.email.trim() != '' && this.validarConsulta()) {
+        if(this.validarEmail() && this.validarNombre()) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+});
+
+app.mount('#app');
 
 function validarCampos(e, form) {
   e.preventDefault();
 
-  const regEx         = {
+  const regEx = {
     alpha  : /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/,
     numeric: /^[0-9]+$/,
     email  : /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/,
